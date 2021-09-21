4f734637233d259e8d4895e5fc6ea2562bb804ce
jake <jakebs540@gmail.com>
2021-09-21 15:03:34 +0100

    refactored after merge

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -12,31 +12,31 @@ class Spaces
 
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
+    if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-results = connection.exec("SELECT * FROM spaces;")
+    results = connection.exec("SELECT * FROM spaces;")
     results.map do |row|
       Spaces.new(name: row['name'], description: row['description'], price: row['price'])
     end
   end
   
-    def self.add(name:, description:, price:)
-     if ENV['RACK_ENV'] == 'test'
-       connection = PG.connect(dbname: 'makersbnb_test')
+  def self.add(name:, description:, price:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-      result = connection.exec("INSERT INTO spaces (name, description, price) VALUES ('#{name}','#{description}','#{price}') RETURNING name, description, price;")
+    result = connection.exec("INSERT INTO spaces (name, description, price) VALUES ('#{name}','#{description}','#{price}') RETURNING name, description, price;")
 
     Spaces.new(
       name: result[0]['name'],
       description: result[0]['description'],
       price: result[0]['price']
     )
-    
   end
+  
 end
     
--- spec/spaces_spec.rb
+++ spec/spaces_spec.rb
@@ -1,13 +1,29 @@
 require 'spaces'
 
 describe Spaces do
-  it 'Can return all spaces' do
-    connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO spaces (name, description, price) VALUES ('loft', 'lovely loft', '1000');")
+  describe '.all' do 
+    it 'Can return all spaces' do
+      connection = PG.connect(dbname: 'makersbnb_test')
+      connection.exec("INSERT INTO spaces (name, description, price) VALUES ('loft', 'lovely loft', '1000');")
 
-    spaces = Spaces.all[0]
-    expect(spaces.name).to eq 'loft'
-    expect(spaces.description).to eq 'lovely loft'
-    expect(spaces.price).to eq '1000'
+      spaces = Spaces.all[0]
+      expect(spaces.name).to eq 'loft'
+      expect(spaces.description).to eq 'lovely loft'
+      expect(spaces.price).to eq '1000'
+    end
+  end
+
+  describe '.add' do 
+    it 'adds a listing' do 
+      space = Spaces.add(name: 'Loft', description: 'A nice loft', price: 90)
+      expect(space).to be_a Spaces
+    end 
+
+    it 'adds the parameters correctly to the database' do 
+      space = Spaces.add(name: 'Loft', description: 'A nice loft', price: 90)
+      expect(space.name).to eq('Loft')
+      expect(space.description).to eq('A nice loft')
+      expect(space.price).to eq('90')
+    end 
   end
 end
