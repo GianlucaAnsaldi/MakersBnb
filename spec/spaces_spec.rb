7c932fd3bd626ec61e5e72f9f6cd4162aa27e019
Tom Lee <tomalee2002@gmail.com>
2021-09-21 17:21:38 +0100

    refactored listings spec

--- spec/spaces_spec.rb
+++ spec/spaces_spec.rb
@@ -4,12 +4,17 @@ describe Spaces do
   describe '.all' do 
     it 'Can return all spaces' do
       connection = PG.connect(dbname: 'makersbnb_test')
+      
       connection.exec("INSERT INTO spaces (name, description, price) VALUES ('loft', 'lovely loft', '1000');")
+      connection.exec("INSERT INTO spaces (name, description, price) VALUES ('house', 'lovely house', '2000');")
+      connection.exec("INSERT INTO spaces (name, description, price) VALUES ('shed', 'lovely shed', '50');")
 
-      spaces = Spaces.all[0]
-      expect(spaces.name).to eq 'loft'
-      expect(spaces.description).to eq 'lovely loft'
-      expect(spaces.price).to eq '1000'
+      spaces = Spaces.all
+      expect(spaces.length).to eq 3
+      expect(spaces.first).to be_a Spaces
+      expect(spaces.first.name).to eq 'loft'
+      expect(spaces.first.description).to eq 'lovely loft'
+      expect(spaces.first.price).to eq '1000'
     end
   end
 
