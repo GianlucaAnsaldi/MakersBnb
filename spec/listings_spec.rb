cf5c9793aba463b08edd1fa1df7e95ddd0d7e8c5
Tom Lee <tomalee2002@gmail.com>
2021-09-21 16:43:16 +0100

    updated feature tests

--- spec/listings_spec.rb
+++ spec/listings_spec.rb
@@ -4,13 +4,17 @@ describe 'spaces' do
   describe '.all' do
     it 'returns all the spaces' do
       connection = PG.connect(dbname: 'makersbnb_test')
-      #spaces = spaces.all
+      
       connection.exec("INSERT INTO spaces (name, description, price) VALUES ('loft', 'lovely loft', '1000');")
       connection.exec("INSERT INTO spaces (name, description, price) VALUES ('house', 'lovely house', '2000');")
       connection.exec("INSERT INTO spaces (name, description, price) VALUES ('shed', 'lovely shed', '50');")
-      connection.exec("INSERT INTO spaces (name, description, price) VALUES ('tent', 'lovely tent', '5000');")
 
-      #expect(spaces.name, spaces.description, spaces.price).to eq
+      spaces = Spaces.all
+      expect(spaces.length).to eq 3
+      expect(spaces.first).to be_a Spaces
+      expect(spaces.first.name).to eq 'loft'
+      expect(spaces.first.description).to eq 'lovely loft'
+      expect(spaces.first.price).to eq '1000'
     end
   end
 end
