3c95b556f4dcaf34144ed70c43d5399862e13a04
Tom Lee <tomalee2002@gmail.com>
2021-09-22 21:24:06 +0100

    refactored filling in user sign up/in info to a web helper method

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,8 +1,7 @@
 feature 'User registration' do
   scenario 'a user can sign up' do
     visit '/'
-    fill_in('email', with: 'email@example.com')
-    fill_in('password', with: 'supersecret123')
+    test_user_info()
     click_button 'Sign up'
     
     expect(current_path).to eq '/listings'
---
+++ spec/web_helpers.rb
@@ -0,0 +1,4 @@
+def test_user_info
+  fill_in('email', with: 'email@example.com')
+  fill_in('password', with: 'supersecret123')
+end
