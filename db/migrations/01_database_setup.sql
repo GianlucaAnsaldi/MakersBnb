930441905680eadb0c3d063abb86de650e771a09
Florence Bain <florence_bain@outlook.com>
2021-09-22 16:27:42 +0100

    updating database

--- db/migrations/01_database_setup.sql
+++ db/migrations/01_database_setup.sql
@@ -3,6 +3,4 @@ CREATE DATABASE makersbnb_test;
 
 CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(280), price INTEGER);
 CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(60));
-CREATE TABLE requests (id SERIAL PRIMARY KEY, requesting_user FOREIGN KEY(id) REFERENCES users(id), space_id FOREIGN KEY(id) REFERENCES spaces(id), approved? BOOLEAN);
-
-id, requesting_user, space_id
+CREATE TABLE requests (id SERIAL PRIMARY KEY, FOREIGN KEY(request_id) REFERENCES users(id), FOREIGN KEY(users_id) REFERENCES spaces(id), approved BOOLEAN);
