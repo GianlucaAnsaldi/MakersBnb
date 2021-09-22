ab43f3cfc7d72ccd2a9a6179e23d6ddae7603d74
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-09-22 11:14:00 +0100

    created feature test for request booking

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -1,10 +1,11 @@
 require 'pg'
 
 class Spaces
-  attr_reader :name, :description, :price
+  attr_reader :id, :name, :description, :price
   
 
-  def initialize(name:, description:, price:)
+  def initialize(id:, name:, description:, price:)
+    @id = id
     @name = name
     @description = description
     @price = price
@@ -19,7 +20,7 @@ class Spaces
     end
     results = connection.exec("SELECT * FROM spaces;")
     results.map do |row|
-      Spaces.new(name: row['name'], description: row['description'], price: row['price'])
+      Spaces.new(id: row['id'], name: row['name'], description: row['description'], price: row['price'])
     end
   end
   
@@ -29,9 +30,10 @@ class Spaces
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
-    result = connection.exec("INSERT INTO spaces (name, description, price) VALUES ('#{name}','#{description}','#{price}') RETURNING name, description, price;")
+    result = connection.exec("INSERT INTO spaces (name, description, price) VALUES ('#{name}','#{description}','#{price}') RETURNING id, name, description, price;")
 
     Spaces.new(
+      id: result[0]['id'],
       name: result[0]['name'],
       description: result[0]['description'],
       price: result[0]['price']
